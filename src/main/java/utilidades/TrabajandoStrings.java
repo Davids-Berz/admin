49e22a767a3c3b2fcb8513e92a8b8e03229cff93
dsaldivar <dsaldivar.workitmx@gmail.com>
2021-02-05 11:26:46 -0600

    add first class TrabajandoStrings

---
+++ src/main/java/utilidades/TrabajandoStrings.java
@@ -0,0 +1,21 @@
+package utilidades;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class TrabajandoStrings {
+
+    public static void main(String[] args) {
+
+
+        /*
+        1.- tomas un substring hasta el primer punto
+        2.- cambiar caracter de espacio por el espacio
+        3.- tomar el valor «sinopsis», (sin caracteres)y guardarlo en una variable
+        * */
+        String frase = "Una sinopsis es un resumen general de una obra en particular.\u200B El término «sinopsis» procede del latín «synopsis», cuyos orígenes se remontan al griego antiguo y que etimológicamente significa visión de conjunto, \u200B de las raíces griegas συν y οψις.";
+
+        String subString = StringUtils.substringAfter(frase, ".");
+
+        System.out.println(subString);
+    }
+}
