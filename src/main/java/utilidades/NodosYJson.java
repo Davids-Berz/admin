96364a56ea5cede5c8c95d1dd5f86dd53964910a
dsaldivar <dsaldivar.workitmx@gmail.com>
2021-02-18 01:59:37 -0600

    add lib jsoup

--- src/main/java/utilidades/NodosYJson.java
+++ src/main/java/utilidades/NodosYJson.java
@@ -3,22 +3,28 @@ package utilidades;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class NodosYJson {
 
     public static void main(String[] args) {
 
-        String json = "{ \"color\" : \"Black\", \"type\" : \"FIAT\" }";
-        JsonNode jsonNode = null;
+        String json = "{\"arrayColores\":[{\"nombreColor\":\"rojo\",\"valorHexadec\":\"#f00\"},{\"nombreColor\":\"verde\",\"valorHexadec\":\"#0f0\"},{\"nombreColor\":\"azul\",\"valorHexadec\":\"#00f\"},{\"nombreColor\":\"cyan\",\"valorHexadec\":\"#0ff\"},{\"nombreColor\":\"negro\",\"valorHexadec\":\"#000\"}]}";
+        JsonNode node = null;
         try {
-            jsonNode = new ObjectMapper().readTree(json);
+            node = new ObjectMapper().readTree(json);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        String color = jsonNode.get("color").asText();
-        String colorAt = jsonNode.at("/color").asText();
 
-        System.out.println("el color es: " + color);
-        System.out.println("el colorAt es: " + colorAt);
+        JsonNode nodeColores = node.at("/arrayColores");
+
+        System.out.println("la suma de todos los colores es: " + nodeColores.size());
+
+        for (JsonNode color: nodeColores){
+            System.out.println(color.at("/nombreColor").asText() + " - " + color.at("/valorHexadec").asText());
+        }
+
     }
 }
