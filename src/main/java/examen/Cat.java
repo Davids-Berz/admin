c2e39c1a75575bf1aff2390de800899fb599efdf
Dsaldivar <dsaldivar.workitmx@gmail.com>
2021-04-08 20:22:11 -0500

    examen banregio java

---
+++ src/main/java/examen/Cat.java
@@ -0,0 +1,13 @@
+package examen;
+
+public class Cat {
+        int x = 50;
+        int y = 100;
+        public static void main(String args[]) {
+            int x = 0, y =10;
+            Cat ex = new Cat ();
+            while (x < 3) {
+            }
+            System.out.println("x = " + x + ", y = " + y);
+        }
+}
