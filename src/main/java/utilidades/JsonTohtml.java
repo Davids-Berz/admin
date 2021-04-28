b2e98457f5fb5e6ca9f219548d950dd10fc236c2
Pepe Gzz <jl.gzz.gzz92@gmail.com>
2021-04-28 09:09:26 -0500

    ajax3

--- src/main/java/utilidades/JsonTohtml.java
+++ src/main/java/utilidades/JsonTohtml.java
@@ -1,13 +1,9 @@
 package utilidades;
 
-<<<<<<< HEAD
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.parser.Parser;
-
-=======
->>>>>>> 7cfdbe6cfe8bd148e42c6236d44cb7270c3638d0
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,12 +15,9 @@ public class JsonTohtml {
     public static void main(String[] args) {
         final String SITE = "";
         //aqui agregas el path donde guardas el archivo json o txt
-<<<<<<< HEAD
+
         Path path = Paths.get("C:/Users/Wiser/Documents/ajax/ajax-json.json");
 
-=======
-        Path path = Paths.get("D:\\David\\tickets\\workit\\cp\\expert\\expert-de.html");
->>>>>>> 7cfdbe6cfe8bd148e42c6236d44cb7270c3638d0
         List<String> str = null;
         try {
             str = Files.readAllLines(path);
@@ -33,7 +26,7 @@ public class JsonTohtml {
         }
 
         // aqui creas un metodo para limpiar el codigo
-<<<<<<< HEAD
+
         String cleanStr  = cleanJson(str);
 
         Document document = Jsoup.parse(cleanStr, "", Parser.htmlParser());
@@ -49,15 +42,6 @@ public class JsonTohtml {
          String body = StringUtils.substringBetween(str.toString(), "\"modules\":\"","\",\"total\"");
          body = StringUtils.replaceAll(body,"\\n", "");
          return body.toString() ;
-=======
-        str  = cleanJson(str);
-
-    }
-
-    static List<String> cleanJson(List<String> str){
-        //to do
-        return str;
->>>>>>> 7cfdbe6cfe8bd148e42c6236d44cb7270c3638d0
     }
 }
 
